16af5303d243b62c7593c60215957f6a4469463f
CarlosSLaureano <carloslaureano1@icloud.com>
2024-12-09 21:54:25 -0300

    Models Commit

---
+++ Models/LivroModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Projeto_teste.Models;
+
+public class LivroModel
+{
+   public int Id { get; set; }
+   public string Título { get; set; }
+   
+   public AutorModel Autor { get; set; }
+
+}
